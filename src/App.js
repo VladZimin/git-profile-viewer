9d2c488ec27d4334f0e407156c895b1aad3f5c21
VladZimin <vlad.zimin1@gmail.com>
2021-12-11 19:45:17 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { ProfileInfo } from './components/ProfileInfo'
+import React from 'react'
+import axios from 'axios'
+import { Heart } from 'react-spinners-css'
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = React.useState('')
+  const [userInfo, setUserInfo] = React.useState({})
+  const [isLoading, setIsLoading] = React.useState(false)
+
+  const location = window.location
+  console.log(location)
+  const his = window.history
+  console.log(his)
+
+  const onChangeInput = (e) => {
+    const { value } = e.target
+    setInputValue(value)
+  }
+  const handleSubmitForm = async (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+    try {
+      const { data } = await axios.get(`https://api.github.com/users/${inputValue}`)
+      setUserInfo(data)
+    } catch (e) {
+      alert('Пользователь не найден')
+      console.log(e.message)
+    }
+    setIsLoading(false)
+    setInputValue('')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app'>
+      <div className='app-container'>
+        <form onSubmit={handleSubmitForm} className='app-form'>
+          <input onChange={onChangeInput} value={inputValue} type='text' className='app-input'
+                 placeholder={isLoading ? 'Загрузка..' : 'Укажите GitHub-аккаунт'} />
+          <button disabled={isLoading} type='submit' className='app-form_btn'>Найти</button>
+        </form>
+        {isLoading ? <Heart className='margin-loader' color='#007afe' size={350} /> : Object.keys(userInfo).length ?
+          <ProfileInfo {...userInfo} /> : null}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
